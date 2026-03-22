c98f3cdb8dd1e62d1628ccf2cfaf362ea196ba50
Eden Fenn <edenfenn@Summoning-Slate.local>
2026-03-22 03:08:47 -0400

    Updates content for March 2026

--- src/About.js
+++ src/About.js
@@ -5,24 +5,33 @@ function About() {
     <section id="About" className="layout">
       <div className="about-content">
         <p>
-          <strong>Eden Fuller</strong> is a front-end web engineer with 8 years
+          <strong>Eden Fuller</strong> is a front-end engineer with over a decade
           of experience building intuitive, reliable, and joyful interfaces for
-          millions of users. Eden is currently the lead storefront engineer at{" "}
-          <strong>Trove</strong>. She's previously worked at{" "}
-          <strong>IKEA</strong>, <strong>AOL</strong>, and{" "}
-          <strong>Brinkbit</strong>.
+          millions of users.
         </p>
+        <p>She's currently a senior software engineer in the Office of Public Outreach at the{" "}
+          <strong>Space Telescope Science Institute</strong>, NASA's astrophysics lab providing operations for the Hubble Space Telescope, James Webb Space Telescope, and Nancy Grace Roman Space Telescope.</p>
         <p>
-          Eden is also a science fiction writer whose short stories have
-          appeared in <em>Nature Futures</em>, <em>Daily Science Fiction</em>,
-          and <em>Asimov's Science Fiction Magazine</em>.
+          Eden has previously worked for orgs including: 
         </p>
+        <ul>
+          <li>
+            <p><strong>Trove:</strong>{` `}Sustainable fashion startup providing resale operations for brands including Levi, Lululemon, REI, and Patagonia </p>
+          </li>
+          <li>
+            <p><strong>IKEA:</strong>{` `}The world's leading furniture retailer, with one of the most-visited e-commerce sites on the Internet</p>
+            </li>
+          <li>
+            <p><strong>FHI 360:</strong>{` `}Global nonprofit working to advance public health and education  </p>
+          </li>
+        </ul>
+        <br />
         <p>
           <strong>
             <em>let's create together //</em>
           </strong>
           <br />
-          <a href="mailto:fuller.eden.m@gmail.com">
+          <a href="mailto:eden.fuller@pm.me">
             <span className="about-link">
               email <img src={outboundBlack}></img>
             </span>
--- src/Home.js
+++ src/Home.js
@@ -7,13 +7,18 @@ function Home() {
       <>
         <div className="about-content">
           <p>
-            <a target="_blank" href="https://www.onward.on-running.com/">
+            <a target="_blank" href="https://spacetelescopelive.org/">
               <span className="about-link">
-                {"On Running Onward "}
+                {"Space Telescope Live "}
                 <img className="outbound-icon" src={outboundBlack} />
               </span>
             </a>
-            {` & `}
+            <br />
+            <strong>Lead front-end engineer, motion designer</strong>
+            <br />
+            {`Interactive web app for NASA on behalf of the Space Telescope Science Institute. Shows observation target and status for the Hubble Space Telescope and James Webb Space Telescope in real time, with information on the research program for each. Calendar and timeline tools enable the public to explore the entire history of over 30,000 JWST observations and 2,000,000 Hubble observations, spanning over 35 years.`}
+          </p>
+          <p>
             <a target="_blank" href="https://generations.canadagoose.com">
               <span className="about-link">
                 {"Canada Goose Generations "}
@@ -21,9 +26,11 @@ function Home() {
               </span>
             </a>
             <br />
-            {`eCommerce sites for the On Running and Canada Goose resale programs.
+            <strong>Lead front-end engineer</strong>
+            <br />
+            {`eCommerce site for the Canada Goose resale program.
             Trove's recommerce partnerships with leading fashion brands diverted
-            over 2MM kg of C02 emissions in 2022. Lead front-end engineer.`}
+            over 2MM kg of C02 emissions in 2022.`}
           </p>
           <p>
             <a
@@ -36,11 +43,12 @@ function Home() {
               </span>
             </a>
             <br />
+            <strong>Lead front-end engineer</strong>
+            <br />
             {`App for creating a Product List Page (PLP) from a list of product
             SKUs that matches the UI and feature set of the PLPs dynamically
             generated for product categories. This gives IKEA's US marketing
-            team increased control over key sales initiatives. Lead front-end
-            engineer.`}
+            team increased control over key sales initiatives.`}
           </p>
           <p>
             <a
@@ -53,9 +61,10 @@ function Home() {
               </span>
             </a>
             <br />
+            <strong>Lead front-end engineer, motion designer</strong>
+            <br />
             {`Joint initiative between FHI360 and Johnson & Johnson to increase
-            interest in STEM professions among middle and high school girls.
-            Lead front-end engineer and animation designer.`}
+            interest in STEM professions among middle and high school girls.`}
           </p>
           <p>
             <a target="_blank" href="https://www.nextengineers.org/">
@@ -65,9 +74,10 @@ function Home() {
               </span>
             </a>
             <br />
+            <strong>Front-end engineer, motion designer</strong>
+            <br />
             {`Developed by FHI360 for a General Electric initiative to increase
-            the diversity of young people in engineering. Designed and developed
-            interative and animated elements.`}
+            the diversity of young people in engineering.`}
           </p>
           <p>
             <a target="_blank" href="https://dress-code.surge.sh/">
@@ -77,7 +87,8 @@ function Home() {
               </span>
             </a>
             <br />
-            <strong>new! </strong>
+            <strong>Front-end engineer, designer (solo project)</strong>
+            <br />
             {`An app I built to experiment with Data-Driven Documents (d3.js). It renders a radar chart that describes the dress code for an event via 5 traits. The chart animates as the user chooses different events from a dropdown.`}
           </p>
         </div>
